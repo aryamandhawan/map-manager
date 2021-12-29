66c07105e415864a24a30829871fce92749ec2f5
Aryaman Dhawan <aryaman.dhawan@adelaide.edu.au>
2021-12-29 22:57:34 +1030

    Revert "Add image points to Mapbox"
    This reverts commit 93a2d40ca322e0686238aafaa47057c08f296af8.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
-import React, { useRef, useEffect, useState } from "react";
-import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import logo from './logo.svg';
+import './App.css';
 
-mapboxgl.accessToken =
-  "***REMOVED***";
-
-export default function App() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-  });
-
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on("move", () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
-
+function App() {
   return (
-    <div>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hello World!
+        </h1>
+      </header>
     </div>
   );
 }
+
+export default App;
