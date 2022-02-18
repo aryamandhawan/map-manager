7ce35dedef3dde47052ca58c74624411f90dde2b
Aryaman Dhawan <aryaman.dhawan@adelaide.edu.au>
2022-02-18 22:44:12 +1030

    set ENV | add carousel

--- backend/mapillary_data/urls.py
+++ backend/mapillary_data/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.views import View
 from . import views
 urlpatterns = [
-    path("", views.react, name="react"),
+    path("", views.index, name="index"),
+    path("app", views.react, name="react"),
     path("api/images/", views.get_image_data, name="image_data")
 ]
